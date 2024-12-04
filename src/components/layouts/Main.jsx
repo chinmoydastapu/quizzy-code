62697e730e433e1e6fd9e3e0dab416feb282c843
Chinmoy Das <chinmoydastapu2@gmail.com>
2024-12-05 01:20:34 +0600

    footer added

--- src/components/layouts/Main.jsx
+++ src/components/layouts/Main.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import HeaderNav from '../HeaderNav/HeaderNav';
+import Footer from '../Footer/Footer';
 
 const Main = () => {
     return (
         <div>
             <HeaderNav></HeaderNav>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
